3f365ac57c66383610ed8417bac368b88f4f1b3b
insuline <insuline.ru@gmail.com>
2023-11-15 13:33:41 +0300

    ♻️ Add signTypedData examples (with viem and ethers)

--- scripts/gen-sig.js
+++ scripts/gen-sig.js
@@ -1,9 +1,12 @@
 const ethers = require('ethers');
+const { hashDomain } = require('viem');
+const { privateKeyToAccount } = require('viem/accounts');
+
 
 async function main() {
 	// example data
 	let domainSeparator =
-		'0x6d10bb011eeb4e5a58d21ece8be3182d8c053ff9f30ece4c93ba12c58a93b7ed';
+		'0x7f9f752009a0fba436a94f6b7c2421e7b480fc4aca9ab399f5cae3a654dd2403';
 	let signerPrivateKey =
 		'1111111111111111111111111111111111111111111111111111111111111111';
 	let marketId = '200';
@@ -23,10 +26,6 @@ async function main() {
 		'115792089237316195423570985008687907853269984665640564039457584007913129639935';
 	let conditions = [];
 
-	// expected signature generated from the above data in Solidity
-	let expectedSignature =
-		'0xaedc3a51d51c3de511aa8d6fb20a1ebb90ea85f71a8157677b1c440d952f910435067505954a4bb21d8cb7d80bc729df10b55a17297511fd918f449349c6e5df1b';
-
 	generateSignature(
 		signerPrivateKey,
 		domainSeparator,
@@ -45,15 +44,233 @@ async function main() {
 		conditions
 	)
 		.then((signature) => {
-			ethers.assert(
-				expectedSignature === signature,
-				'Signatures do not match'
-			);
-			console.log('Signature:', signature);
+			console.log('Signature with signingKey:', signature);
 		})
 		.catch((error) => {
 			console.error('Error signing message:', error);
 		});
+
+	generateSignatureViemTypedData(
+		signerPrivateKey,
+		domainSeparator,
+		marketId,
+		accountId,
+		sizeDelta,
+		settlementStrategyId,
+		acceptablePrice,
+		isReduceOnly,
+		trackingCode,
+		referrer,
+		nonce,
+		requireVerified,
+		trustedExecutor,
+		maxExecutorFee,
+		conditions
+	)
+		.then((signature) => {
+			console.log('Signature with viem signTypedData:', signature);
+		})
+		.catch((error) => {
+			console.error('Error signing message:', error);
+		});
+
+	generateSignatureTypedData(
+		signerPrivateKey,
+		domainSeparator,
+		marketId,
+		accountId,
+		sizeDelta,
+		settlementStrategyId,
+		acceptablePrice,
+		isReduceOnly,
+		trackingCode,
+		referrer,
+		nonce,
+		requireVerified,
+		trustedExecutor,
+		maxExecutorFee,
+		conditions
+	)
+		.then((signature) => {
+			console.log('Signature with ethers signTypedData:', signature);
+		})
+		.catch((error) => {
+			console.error('Error signing message:', error);
+		});
+}
+
+async function generateSignatureViemTypedData(
+	pk, // private key,
+	domainSeparator, // can get this from the engine contract (i.e. engine.DOMAIN_SEPARATOR())
+	marketId,
+	accountId,
+	sizeDelta,
+	settlementStrategyId,
+	acceptablePrice,
+	isReduceOnly,
+	trackingCode,
+	referrer,
+	nonce,
+	requireVerified,
+	trustedExecutor,
+	maxExecutorFee,
+	conditions
+) {
+	const account = privateKeyToAccount(pk)
+	const signer = account.address
+	console.log(signer)
+
+	let orderDetails = {
+		marketId: BigInt(marketId),
+		accountId: BigInt(accountId),
+		sizeDelta: BigInt(sizeDelta),
+		settlementStrategyId: BigInt(settlementStrategyId),
+		acceptablePrice: BigInt(acceptablePrice),
+		isReduceOnly: isReduceOnly,
+		trackingCode: trackingCode,
+		referrer: referrer,
+	};
+
+	// define the conditional order struct
+	let conditionalOrder = {
+		orderDetails: orderDetails,
+		signer: signer,
+		nonce: BigInt(nonce),
+		requireVerified: requireVerified,
+		trustedExecutor: trustedExecutor,
+		maxExecutorFee: BigInt(maxExecutorFee),
+		conditions: conditions,
+	};
+
+	const domain = hashDomain({
+		domain: {
+			name: 'SMv3: OrderBook',
+			version: '1',
+			chainId: '84531',
+			verifyingContract: '0x13eBcd3aD7B4Aa0595Fe57085C62c4f9CaDdB000',
+		},
+		types: {
+			EIP712Domain: [
+				{ name: 'name', type: 'string'},
+				{ name: 'version', type: 'string'},
+				{ name: 'chainId', type: 'uint256'},
+				{ name: 'verifyingContract', type: 'address'},
+			]
+		}
+	})
+
+	console.log(domain === domainSeparator)
+
+	const signed = account.signTypedData({
+		domain: {
+			name: 'SMv3: OrderBook',
+			version: '1',
+			chainId: '84531',
+			verifyingContract: '0x13eBcd3aD7B4Aa0595Fe57085C62c4f9CaDdB000',
+		},
+		types: {
+				OrderDetails: [
+					{ name: 'marketId', type: 'uint128' },
+					{ name: 'accountId', type: 'uint128' },
+					{ name: 'sizeDelta', type: 'int128' },
+					{ name: 'settlementStrategyId', type: 'uint128' },
+					{ name: 'acceptablePrice', type: 'uint256' },
+					{ name: 'isReduceOnly', type: 'bool' },
+					{ name: 'trackingCode', type: 'bytes32' },
+					{ name: 'referrer', type: 'address' },
+				],
+				ConditionalOrder: [
+					{ name: 'orderDetails', type: 'OrderDetails' },
+					{ name: 'signer', type: 'address' },
+					{ name: 'nonce', type: 'uint256' },
+					{ name: 'requireVerified', type: 'bool' },
+					{ name: 'trustedExecutor', type: 'address' },
+					{ name: 'maxExecutorFee', type: 'uint256' },
+					{ name: 'conditions', type: 'bytes[]' },
+				],
+		},
+		primaryType: "ConditionalOrder",
+		message: {
+			...conditionalOrder
+		}
+	})
+
+	return signed
+}
+
+async function generateSignatureTypedData(
+	pk, // private key,
+	domainSeparator, // can get this from the engine contract (i.e. engine.DOMAIN_SEPARATOR())
+	marketId,
+	accountId,
+	sizeDelta,
+	settlementStrategyId,
+	acceptablePrice,
+	isReduceOnly,
+	trackingCode,
+	referrer,
+	nonce,
+	requireVerified,
+	trustedExecutor,
+	maxExecutorFee,
+	conditions
+) {
+	const wallet = new ethers.Wallet(pk);
+	const signer = wallet.address;
+
+	let orderDetails = {
+		marketId: BigInt(marketId),
+		accountId: BigInt(accountId),
+		sizeDelta: BigInt(sizeDelta),
+		settlementStrategyId: BigInt(settlementStrategyId),
+		acceptablePrice: BigInt(acceptablePrice),
+		isReduceOnly: isReduceOnly,
+		trackingCode: trackingCode,
+		referrer: referrer,
+	};
+
+	// define the conditional order struct
+	let conditionalOrder = {
+		orderDetails: orderDetails,
+		signer: signer,
+		nonce: BigInt(nonce),
+		requireVerified: requireVerified,
+		trustedExecutor: trustedExecutor,
+		maxExecutorFee: BigInt(maxExecutorFee),
+		conditions: conditions,
+	};
+
+	const signed = await wallet.signTypedData({
+			name: 'SMv3: OrderBook',
+			version: '1',
+			chainId: '84531',
+			verifyingContract: '0x13eBcd3aD7B4Aa0595Fe57085C62c4f9CaDdB000',
+		},
+		{
+				OrderDetails: [
+					{ name: 'marketId', type: 'uint128' },
+					{ name: 'accountId', type: 'uint128' },
+					{ name: 'sizeDelta', type: 'int128' },
+					{ name: 'settlementStrategyId', type: 'uint128' },
+					{ name: 'acceptablePrice', type: 'uint256' },
+					{ name: 'isReduceOnly', type: 'bool' },
+					{ name: 'trackingCode', type: 'bytes32' },
+					{ name: 'referrer', type: 'address' },
+				],
+				ConditionalOrder: [
+					{ name: 'orderDetails', type: 'OrderDetails' },
+					{ name: 'signer', type: 'address' },
+					{ name: 'nonce', type: 'uint256' },
+					{ name: 'requireVerified', type: 'bool' },
+					{ name: 'trustedExecutor', type: 'address' },
+					{ name: 'maxExecutorFee', type: 'uint256' },
+					{ name: 'conditions', type: 'bytes[]' },
+				],
+		},
+		conditionalOrder
+	)
+
+	return signed
 }
 
 async function generateSignature(
